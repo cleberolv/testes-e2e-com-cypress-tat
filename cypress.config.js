843b5eb450d40d603d7a7b27105f14540e68201b
Cleber Oliveira <oliveira.bnhc@gmail.com>
2024-05-29 01:20:11 -0300

    cypress Cloud configs

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,12 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+	chromeWebSecurity: false,
 	e2e: {
 		baseUrl: 'https://notes-serverless-app.com',
-		setupNodeEvents(on, config) {
-			// implement node event listeners here
-		},
 		env: {
 			viewportWidthBreakpoint: 768,
 		}
 	},
-	chromeWebSecurity: false,
+	projectId: 'gn21t3'
 })
