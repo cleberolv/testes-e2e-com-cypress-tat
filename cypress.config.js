b6b2cf532b5ce7ce9a561b5bc29b9dda2b0d621b
Cleber Oliveira <oliveira.bnhc@gmail.com>
2024-05-30 01:25:25 -0300

    Configure cypress grep

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,11 @@ module.exports = defineConfig({
 		baseUrl: 'https://notes-serverless-app.com',
 		env: {
 			viewportWidthBreakpoint: 768,
-		}
+		},
+		setupNodeEvents(on, config) {
+			require('@cypress/grep/src/plugin')(config)
+			return config
+		},
 	},
 	projectId: 'gn21t3',
 })
